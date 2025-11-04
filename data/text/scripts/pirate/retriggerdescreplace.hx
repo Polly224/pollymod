4512c58f993162effd0612f5303474985e3559eb
LauraOosthoek <144798759+Polly224@users.noreply.github.com>
2025-11-04 19:24:31 +0100

    updated descreplace

--- data/text/scripts/pirate/retriggerdescreplace.hx
+++ data/text/scripts/pirate/retriggerdescreplace.hx
@@ -1,16 +1,14 @@
 var wordcheck = args[0];
 var wordreplace = args[1];
-for (eq in self.equipment)
+var eq = args[2];
+var index = eq.fulldescription.indexOf(wordcheck);
+if (index != -1)
 {
-    var index = eq.fulldescription.indexOf(wordcheck);
-    if (index != -1)
+    var newdesc = eq.fulldescription.split(wordcheck);
+    eq.fulldescription = newdesc[0];
+    for (i in 0...(newdesc.length - 1))
     {
-        var newdesc = eq.fulldescription.split(wordcheck);
-        eq.fulldescription = newdesc[0];
-        for (i in 0...(newdesc.length - 1))
-        {
-            eq.fulldescription += wordreplace + newdesc[i + 1];
-        }
-        eq.animate("flashandshake");
+        eq.fulldescription += wordreplace + newdesc[i + 1];
     }
+    eq.animate("flashandshake");
 }
--- data/text/scripts/pirate/reusetoretriggerdescreplace.hx
+++ data/text/scripts/pirate/reusetoretriggerdescreplace.hx
@@ -1,6 +1,28 @@
-runscript("pirate/retriggerdescreplace", ["recycle", "placeholder"]);
-runscript("pirate/retriggerdescreplace", ["re-use", "placeholdy"]);
-runscript("pirate/retriggerdescreplace", ["re-trigger", "re-use"]);
-runscript("pirate/retriggerdescreplace", ["retrigger", "recycle"]);
-runscript("pirate/retriggerdescreplace", ["placeholder", "retrigger"]);
-runscript("pirate/retriggerdescreplace", ["placeholdy", "re-trigger"]);
+var swappedeq = [];
+if(self.varexists("descreuseswappedeq")) swappedeq = self.getvar("descreuseswappedeq");
+var swapall = true;
+if(args.length > 0) swapall = args[0];
+
+if(swapall){
+    for(eq in self.equipment){
+        if(swappedeq.indexOf(eq) != -1) swappedeq.remove(eq);
+        else swappedeq.push(eq);
+        runscript("pirate/retriggerdescreplace", ["recycle", "placeholder", eq]);
+        runscript("pirate/retriggerdescreplace", ["re-use", "placeholdy", eq]);
+        runscript("pirate/retriggerdescreplace", ["re-trigger", "re-use", eq]);
+        runscript("pirate/retriggerdescreplace", ["retrigger", "recycle", eq]);
+        runscript("pirate/retriggerdescreplace", ["placeholder", "retrigger", eq]);
+        runscript("pirate/retriggerdescreplace", ["placeholdy", "re-trigger", eq]);
+    }
+    self.setvar("descreuseswappedeq", swappedeq);
+} else{
+    for(eq in swappedeq){
+        runscript("pirate/retriggerdescreplace", ["recycle", "placeholder", eq]);
+        runscript("pirate/retriggerdescreplace", ["re-use", "placeholdy", eq]);
+        runscript("pirate/retriggerdescreplace", ["re-trigger", "re-use", eq]);
+        runscript("pirate/retriggerdescreplace", ["retrigger", "recycle", eq]);
+        runscript("pirate/retriggerdescreplace", ["placeholder", "retrigger", eq]);
+        runscript("pirate/retriggerdescreplace", ["placeholdy", "re-trigger", eq]);
+    }
+    self.resetvar("descreuseswappedeq");
+}
