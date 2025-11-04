d5dd7ac8c2574d7c56e8d74c0eea263cf2ebd7ea
LauraOosthoek <144798759+Polly224@users.noreply.github.com>
2025-11-04 22:15:23 +0100

    buncha stuff

--- data/text/scripts/pirate/reinyarrrnation.hx
+++ data/text/scripts/pirate/reinyarrrnation.hx
@@ -1,21 +1,17 @@
 var self = args[0];
 var e = args[1];
 var upgraded = args[2];
-var eqlist = upgraded ? (self.spaceleft() > 1 ? self.getvar("bigrandomequipfilter@u") : self.getvar("randomequipfilter@u")) : (self.spaceleft() > 1 ? self.getvar("bigrandomequipfilter") : self.getvar("randomequipfilter"));
+var eqlist = upgraded ? self.getvar("randomequipfilter@u") : self.getvar("randomequipfilter");
 var modifier = upgraded ? "+" : "";
 var pickedname = rand(eqlist);
 
 var eq = new elements.Equipment(pickedname + modifier);
 _internalexchangeequipment(self, e, eq, -1);
 delay(0.5);
-eq.scriptendturn = "e.animate('fastdestroy'); giveequipment('" + e.name + modifier + "', true, false, e); for(eq in self.equipment){if(eq.name == 'Reinyarrrnation'){eq.x = -50000; eq.finalpos.x = -50000; animateequipmentintoplace(self);}}";
+eq.scriptendturn = "e.animate('fastdestroy'); giveequipment('" + e.name + modifier + "', true, false, e);animateequipmentintoplace(self);}}";
 var temptest = new elements.Equipment(pickedname + modifier);
-if(temptest.size == 2)
-{
-    eq.resize(2);
-    eq.finalpos.y = 630;
-    animateequipmentintoplace(self);
-}
+eq.finalpos.y = e.finalpos.y;
+if(!self.isplayer) eq.castdirection *= -1;
 
 var finalvals = [];
 for (i in eq.slots)
