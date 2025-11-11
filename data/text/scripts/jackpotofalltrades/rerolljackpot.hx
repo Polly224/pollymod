d4a0ea3b5d5061c34e4def575199aa15fdf8c159
LauraOosthoek <144798759+Polly224@users.noreply.github.com>
2025-11-11 11:44:32 +0100

    updated rerolljackpot

--- data/text/scripts/jackpotofalltrades/rerolljackpot.hx
+++ data/text/scripts/jackpotofalltrades/rerolljackpot.hx
@@ -1,5 +1,5 @@
-var JackpotOptions = ["Duplicate", "Midnight Snake", "Sandvich", "Suit Up", "Slingshot", "Syringe", "Lucky Roll", "Prickly Pear", "Sniper Shot", "Melting Ice", "Firey Roll", "Faulty Wiring", "Icy Roll", "Whoopie Cushion", "Reshuffle"];
-var RareJackpotOptions = ["Stored Prize", "Split Path"];
+var JackpotOptions = getcolumn(loaddata("jackpotofalltrades/leveljackpotskills"), "n" + self.level);
+var RareJackpotOptions = getcolumn(loaddata("jackpotofalltrades/leveljackpotskills"),"r" + self.level);
 var UpgradeAll = false;
 var IsReroll = false;
 var SkillsCount = 3;
@@ -19,84 +19,8 @@ if(args.length == 2)
 	IsReroll = args[1];
 }
 
-var ctdowncheck = false;
-for(eq in self.equipment)
-{
-	if(eq.slots.indexOf("COUNTDOWN") != -1)
-	{
-		ctdowncheck = true;
-	}
-}
-if (ctdowncheck)
-{
-	JackpotOptions.push("Final Countdown");
-}
 
-if(self.level > 1) 
-{
-	JackpotOptions = JackpotOptions.concat(["Debatable Damage","Flamethrower","Stress","Salvaging","Slippery Shoes","Borrow","Mechanical Toxin","Curseathon", "Used Syringe", "The 50/50", "Guarded Fling"]);
-	RareJackpotOptions = RareJackpotOptions.concat(["Fifths Roll", "Containment Wave"]);
-	var diceamount = 0;
-}    
 
-if(self.level > 2) 
-{
-	JackpotOptions = JackpotOptions.concat(["Leech", "Last Resort","Timebomb","Ctrl + C","Nanomachines", "Ice Age", "PC Fans"]);
-	RareJackpotOptions = RareJackpotOptions.concat(["Limit Break","Lifeblood"]);
-	
-	if(self.limitbreak.name.indexOf("Autoroll") < 0)
-	{
-		JackpotOptions.push("Manual Roll");
-	}
-	var RemoveOptions2 = ["Final Countdown", "Duplicate", "Stress", "Icy Roll", "Syringe", "Prickly Pear", "Slingshot", "Suit Up", "Sandvich", "Melting Ice", "Faulty Wiring"];
-	for(i in RemoveOptions2)
-	{
-		JackpotOptions.remove(i);
-	}
-}
-
-if(self.level > 3) 
-{
-	JackpotOptions = JackpotOptions.concat(["Headshot","Lock Advocate","Dragon's Roll","Tri Attack","Basil's Protection","Blood Arrow","Persona"]);
-	
-	if(target.innate.length > 0)
-	{
-		RareJackpotOptions.push("Uninnate");
-	}
-	JackpotOptions.remove("Lucky Roll");
-	JackpotOptions.remove("Used Syringe");
-    JackpotOptions.remove("Sniper Shot");
-    JackpotOptions.remove("Flamethrower");
-    JackpotOptions.remove("The 50/50");
-    JackpotOptions.remove("Firey Roll");
-    JackpotOptions.remove("Guarded Fling");
-    JackpotOptions.remove("Manual Roll");
-    JackpotOptions.remove("Mechanical Toxin");
-    RareJackpotOptions.remove("Fifths Roll");
-    RareJackpotOptions = RareJackpotOptions.concat(["Feed", "Cthulhu's Shield", "Perfect Spanner", "Ka... Ching!"]);
-
-	if (self.getvar("firstturnpassed") && (self.getvar("currentroundplayerhp") - self.hp) > (self.getvar("currentroundenemyhp") - target.hp)) 
-	{
-		RareJackpotOptions.push("Stars And Time");
-	}
-}
-
-if(self.level > 4) 
-{
-	JackpotOptions.remove("Borrow");
-    JackpotOptions.remove("Midnight Snake");
-    JackpotOptions = JackpotOptions.concat(["Mad Dog's Howl","Prince's Steal", "Jimbo's Trick", "Double Roll"]);
-	if(self.hp < self.maxhp / 2)
-	{
-		RareJackpotOptions.push("Dead Ringer");
-	}
-	RareJackpotOptions.push("The Blue Devil");
-}    
-
-if(self.level == 6) 
-{
-	RareJackpotOptions = RareJackpotOptions.concat(["Botulinum Dose", "Run It Back"]);
-}
 var ChosenJackpotSkills = [];
 
 for(i in 0...SkillsCount)
